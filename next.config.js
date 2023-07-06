e5ecada0e01d03f4281f58e8ebe19d78d81e64fc
ABHINAV ANURAG <109911339+abhinav70291@users.noreply.github.com>
2023-07-06 14:55:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      appDir: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    // Remove the invalid 'images' property from the 'experimental' field
+  },
+  // Use the 'output: export' option as suggested by the error message
+  output: 'export',
+};
+
+module.exports = nextConfig;
